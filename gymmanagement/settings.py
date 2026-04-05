656094de47ed6fcb7987794ce2662c5fba66035e
Rahul <rahulrajps007c@gmail.com>
2026-04-05 08:24:47 +0530

    edit staticfiles root  for static files

--- gymmanagement/settings.py
+++ gymmanagement/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT=BASE_DIR/'media'
 MEDIA_URL='/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
